df4f9eb9e4417976feba6ca62942ef01526e1022
skjtcodes <shawn.work.script@gmail.com>
2021-12-11 20:52:08 +0800

    permission error

--- updateComic/run.sh
+++ updateComic/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running Script"
-python3 ~/Source_Code/WebLibrary/updateComic/main.py --input ~/Database/H/ --prod
+python3 ~/Source_Code/WebLibrary/updateComic/main.py --input ~/Database/H/ --output ~/Source_Code/WebLibrary/backend/public/comic/ --prod
