1711f024964730659c56d0397ebd3915b0f05e5f
skjtcodes <shawn.work.script@gmail.com>
2021-12-19 14:42:55 +0800

    update run.sh

--- updateComic/run.sh
+++ updateComic/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running Script"
-python3 ~/Source_Code/WebLibrary/updateComic/main.py --input ~/Database/H/ --output ~/Source_Code/WebLibrary/backend/public/comic/ --prod
+python ~/Source_Code/WebLibrary/updateComic/main.py --input ~/Database/H/ --output ~/Source_Code/WebLibrary/backend/public/comic/ --prod
